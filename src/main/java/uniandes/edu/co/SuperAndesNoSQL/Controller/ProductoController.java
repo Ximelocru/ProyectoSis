3b82253a2813ac6d03db921763fb8ccd40166ef8
ximelocru <ax.lopez@uniandes.edu.co>
2024-12-01 23:04:50 -0500

    p

--- src/main/java/uniandes/edu/co/SuperAndesNoSQL/Controller/ProductoController.java
+++ src/main/java/uniandes/edu/co/SuperAndesNoSQL/Controller/ProductoController.java
@@ -1,5 +1,7 @@
 package uniandes.edu.co.SuperAndesNoSQL.controller;
 
+import java.util.Collection;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +29,7 @@ public class ProductoController {
             if (productoRepository.existsById(producto.getId())) {
                 return new ResponseEntity<>("El ID del producto ya existe", HttpStatus.BAD_REQUEST);
             }
-            if (productoRepository.buscarPorCodigoBarras(producto.getCodBarras()) != null) {
+            if (productoRepository.buscarPorCodBarras(producto.getCodBarras()) != null) {
                 return new ResponseEntity<>("El código de barras del producto ya existe", HttpStatus.BAD_REQUEST);
             }
             productoRepository.save(producto);
@@ -37,4 +39,48 @@ public class ProductoController {
                     HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+  
+
+    @GetMapping("/codbarras/{codigoBarras}")
+    public ResponseEntity<Producto> obtenerProductoPorCodigoBarras(@PathVariable Integer codigoBarras) {
+        try {
+            Producto producto = productoRepository.buscarPorCodBarras(codigoBarras);
+            if (producto != null) {
+                return ResponseEntity.ok(producto);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+    @GetMapping("/nombre/{nombre}")
+    public ResponseEntity<Collection<Producto>> obtenerProductoPorNombre(@PathVariable String nombre) {
+        try {
+            List<Producto> producto = productoRepository.buscarPorNombre(nombre);
+            return ResponseEntity.ok(producto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+    @PostMapping("/{codBarras}/edit/save")
+    public ResponseEntity<String> actualizarProducto(@PathVariable("codBarras") Integer codBarras, @RequestBody Producto producto) {
+        try {
+            productoRepository.actualizarProducto(
+                producto.getCodBarras(),
+                producto.getNombre(),
+                producto.getPrecioUnitarioVenta(),
+                producto.getPresentacion(),
+                producto.getCantidadPresentacio(),
+                producto.getUnidadMedia(),
+                producto.getFechaExpiracion(),
+                producto.getid_expecificacionesEmpacado(),
+                producto.getId_categoria()
+            );
+            return new ResponseEntity<>("Bar actualizado exitosamente", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al actualizar el bar: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
